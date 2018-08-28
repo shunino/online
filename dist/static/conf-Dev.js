ef22a990bf44e10325da034ee8f8704f3468b79c
shuxing <wb-sx363131@alibaba-inc.com>
2018-08-28 16:34:08 +0800

    update

--- dist/static/conf-Dev.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  server: "http://172.19.50.37:8080/rest/api",
-  file_show_url:"http://chudian.oss-cn-hangzhou.aliyuncs.com/"
-}
--- dist/static/conf-QA.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  server: "http://salewxqa.gtdreamlife.com/rest/api",
-  file_show_url:"http://chudian.oss-cn-hangzhou.aliyuncs.com/"
-}
